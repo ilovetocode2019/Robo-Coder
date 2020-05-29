8fd08cf0f05c369346304012072f8e4912dd83b7
Nathan <nathanwj123@outlook.com>
2020-05-29 12:07:14 -0700

    Update fun

--- cogs/fun.py
+++ cogs/fun.py
@@ -9,27 +9,6 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="reaction", description="Give the user a reaction", usage="[memberid] [emoji]")
-    async def reaction(self, ctx, arg1, arg2):
-        msg = await ctx.channel.fetch_message(int(arg1))
-        emoji = discord.utils.get(self.bot.emojis, name=arg2)
-        try:
-            await msg.add_reaction(str(emoji))
-            await ctx.send("Message reacted with: " + str(emoji))
-        except:
-            await msg.add_reaction(str(arg2))
-            await ctx.send("Message reacted with: " + str(arg2))
-
-
-    @commands.command(name="poll", description="Create a poll", usage="'[poll msg]' [reacton(s)]")
-    async def poll(self, ctx, msg, *reactions):
-        await ctx.send("Created new poll")
-        em=discord.Embed(title="POLL")
-        em.add_field(name=ctx.author, value=msg, inline=False)
-        msg = await ctx.send(embed=em)
-        for emoji in reactions:
-            await msg.add_reaction(emoji)
-
     @commands.command(name="emoji", description="Find an emoji from any server the bot is on", usage="[emoji name]")
     async def detectemoji(self, ctx, *, emojitxt):
         emoji = discord.utils.get(self.bot.emojis, name=emojitxt)
@@ -38,11 +17,18 @@ class Fun(commands.Cog):
     
     @commands.command(name="flipcoin", description="Flip a coin")
     async def flipcoin(self, ctx):
-        await ctx.send(f"You got a {random.choice(['Head', 'Tail'])}")
+        await ctx.send(f"You got a {random.choice(['head', 'tail'])}")
 
     @commands.command(name="8ball", description="Use a 8 ball")
     async def magic8ball(self, ctx):
         await ctx.send(f"{random.choice(['Yes', 'No', 'Maybe'])}")
+    
+    @commands.command(name="embed", description="Create a simple embed")
+    async def embed(self, ctx, title, *, msg):
+        em = discord.Embed(title=title, description=msg, color=0x00ff00)
+        em.set_author(name=str(ctx.author.display_name), icon_url=ctx.author.avatar_url)
+        em.set_footer(text=self.bot.user.name, icon_url=self.bot.user.avatar_url)
+        msg = await ctx.send(embed=em)
 
 def setup(bot):
     bot.add_cog(Fun(bot))
