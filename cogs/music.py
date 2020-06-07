b806527cc6c70fa46b5872053259510530233677
ilovetocode2019 <54194229+ilovetocode2019@users.noreply.github.com>
2020-06-07 09:10:38 -0700

    Remove youtube-dl import exception catch

--- cogs/music.py
+++ cogs/music.py
@@ -12,12 +12,7 @@ from aiohttp import ClientSession
 from io import BytesIO
 import random
 
-try:
-    import youtube_dl
-    youtube_dl_imported = True
-except ModuleNotFoundError:
-    youtube_dl_imported = False
-    print("Youtube-dl is not found")
+import youtube_dl
 
 import re
 from datetime import datetime as d
@@ -53,224 +48,223 @@ class Song():
     def __str__(self):
         return self.song
 
-if youtube_dl_imported:
-    class YTDLSource:
-        YTDL_OPTIONS = {
-            'format': 'bestaudio/best',
-            'extractaudio': True,
-            'audioformat': 'mp3',
-            'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-            'restrictfilenames': True,
-            'noplaylist': True,
-            'nocheckcertificate': True,
-            'ignoreerrors': False,
-            'logtostderr': False,
-            'quiet': True,
-            'no_warnings': True,
-            'default_search': 'auto',
-            'source_address': '0.0.0.0',
-        }
+class YTDLSource:
+    YTDL_OPTIONS = {
+        'format': 'bestaudio/best',
+        'extractaudio': True,
+        'audioformat': 'mp3',
+        'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+        'restrictfilenames': True,
+        'noplaylist': True,
+        'nocheckcertificate': True,
+        'ignoreerrors': False,
+        'logtostderr': False,
+        'quiet': True,
+        'no_warnings': True,
+        'default_search': 'auto',
+        'source_address': '0.0.0.0',
+    }
 
-        FFMPEG_OPTIONS = {
-            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-            'options': '-vn',
-        }
+    FFMPEG_OPTIONS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+        'options': '-vn',
+    }
 
-        ytdl = youtube_dl.YoutubeDL(YTDL_OPTIONS)
+    ytdl = youtube_dl.YoutubeDL(YTDL_OPTIONS)
 
-        def __init__(self, ctx: commands.Context, *, data: dict, volume: float = 0.5):
+    def __init__(self, ctx: commands.Context, *, data: dict, volume: float = 0.5):
 
-            self.data = data
+        self.data = data
 
-            self.uploader = data.get('uploader')
-            self.uploader_url = data.get('uploader_url')
-            date = data.get('upload_date')
-            self.upload_date = date[6:8] + '/' + date[4:6] + '/' + date[0:4]
-            self.title = data.get('title')
-            self.thumbnail = data.get('thumbnail')
-            self.description = data.get('description')
-            self.duration = self.parse_duration(int(data.get('duration')))
-            self.tags = data.get('tags')
-            self.url = data.get('webpage_url')
-            self.views = data.get('view_count')
-            self.likes = data.get('like_count')
-            self.dislikes = data.get('dislike_count')
-            self.stream_url = data.get('url')
+        self.uploader = data.get('uploader')
+        self.uploader_url = data.get('uploader_url')
+        date = data.get('upload_date')
+        self.upload_date = date[6:8] + '/' + date[4:6] + '/' + date[0:4]
+        self.title = data.get('title')
+        self.thumbnail = data.get('thumbnail')
+        self.description = data.get('description')
+        self.duration = self.parse_duration(int(data.get('duration')))
+        self.tags = data.get('tags')
+        self.url = data.get('webpage_url')
+        self.views = data.get('view_count')
+        self.likes = data.get('like_count')
+        self.dislikes = data.get('dislike_count')
+        self.stream_url = data.get('url')
 
 
-        def __str__(self):
-            return '**{0.title}** by **{0.uploader}**'.format(self)
+    def __str__(self):
+        return '**{0.title}** by **{0.uploader}**'.format(self)
 
-        @classmethod
-        async def create_source(cls, ctx: commands.Context, search: str, *, loop: asyncio.BaseEventLoop = None):
-            loop = loop or asyncio.get_event_loop()
-            
-            if search.startswith("https:"):
-                await ctx.send(f"🔎 Searching <{search}>")
-            else:
-                await ctx.send(f"🔎 Searching {search}")
+    @classmethod
+    async def create_source(cls, ctx: commands.Context, search: str, *, loop: asyncio.BaseEventLoop = None):
+        loop = loop or asyncio.get_event_loop()
+        
+        if search.startswith("https:"):
+            await ctx.send(f"🔎 Searching <{search}>")
+        else:
+            await ctx.send(f"🔎 Searching {search}")
 
-            partial = functools.partial(cls.ytdl.extract_info, search, download=False, process=False)
-            data = await loop.run_in_executor(None, partial)
+        partial = functools.partial(cls.ytdl.extract_info, search, download=False, process=False)
+        data = await loop.run_in_executor(None, partial)
 
-            if data is None:
+        if data is None:
+            raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
+
+        if 'entries' not in data:
+            process_info = data
+        else:
+            process_info = None
+            for entry in data['entries']:
+                if entry:
+                    process_info = entry
+                    break
+
+            if process_info is None:
+                raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
+        
+
+        webpage_url = process_info['webpage_url']
+
+        partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=True)
+        processed_info = await loop.run_in_executor(None, partial)
+
+        if processed_info is None:
+            raise YTDLError('Couldn\'t fetch `{}`'.format(webpage_url))
+
+        if 'entries' not in processed_info:
+            info = processed_info
+        else:
+            info = None
+            while info is None:
+                try:
+                    info = processed_info['entries'].pop(0)
+                except IndexError:
+                    raise YTDLError('Couldn\'t retrieve any matches for `{}`'.format(webpage_url))
+
+        return cls.ytdl.prepare_filename(info), cls(ctx, data=info)
+
+    @staticmethod
+    def parse_duration(duration: int):
+        minutes, seconds = divmod(duration, 60)
+        hours, minutes = divmod(minutes, 60)
+        days, hours = divmod(hours, 24)
+
+        duration = []
+        if days > 0:
+            duration.append('{} days'.format(days))
+        if hours > 0:
+            duration.append('{} hours'.format(hours))
+        if minutes > 0:
+            duration.append('{} minutes'.format(minutes))
+        if seconds > 0:
+            duration.append('{} seconds'.format(seconds))
+
+        return ', '.join(duration)
+
+class YTDLInfo:
+    YTDL_OPTIONS = {
+        'format': 'bestaudio/best',
+        'extractaudio': True,
+        'audioformat': 'mp3',
+        'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+        'restrictfilenames': True,
+        'noplaylist': True,
+        'nocheckcertificate': True,
+        'ignoreerrors': False,
+        'logtostderr': False,
+        'quiet': True,
+        'no_warnings': True,
+        'default_search': 'auto',
+        'source_address': '0.0.0.0',
+    }
+
+    FFMPEG_OPTIONS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+        'options': '-vn',
+    }
+
+    ytdl = youtube_dl.YoutubeDL(YTDL_OPTIONS)
+
+    def __init__(self, ctx: commands.Context, *, data: dict, volume: float = 0.5):
+
+        self.data = data
+
+        self.uploader = data.get('uploader')
+        self.uploader_url = data.get('uploader_url')
+        date = data.get('upload_date')
+        self.upload_date = date[6:8] + '/' + date[4:6] + '/' + date[0:4]
+        self.title = data.get('title')
+        self.thumbnail = data.get('thumbnail')
+        self.description = data.get('description')
+        self.duration = self.parse_duration(int(data.get('duration')))
+        self.tags = data.get('tags')
+        self.url = data.get('webpage_url')
+        self.views = data.get('view_count')
+        self.likes = data.get('like_count')
+        self.dislikes = data.get('dislike_count')
+        self.stream_url = data.get('url')
+
+    def __str__(self):
+        return '**{0.title}** by **{0.uploader}**'.format(self)
+
+    @classmethod
+    async def create_source(cls, ctx: commands.Context, search: str, *, loop: asyncio.BaseEventLoop = None):
+        loop = loop or asyncio.get_event_loop()
+
+        partial = functools.partial(cls.ytdl.extract_info, search, download=False, process=False)
+        data = await loop.run_in_executor(None, partial)
+
+        if data is None:
+            raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
+
+        if 'entries' not in data:
+            process_info = data
+        else:
+            process_info = None
+            for entry in data['entries']:
+                if entry:
+                    process_info = entry
+                    break
+
+            if process_info is None:
                 raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
 
-            if 'entries' not in data:
-                process_info = data
-            else:
-                process_info = None
-                for entry in data['entries']:
-                    if entry:
-                        process_info = entry
-                        break
+        webpage_url = process_info['webpage_url']
+        partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=False)
+        processed_info = await loop.run_in_executor(None, partial)
 
-                if process_info is None:
-                    raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
-            
+        if processed_info is None:
+            raise YTDLError('Couldn\'t fetch `{}`'.format(webpage_url))
 
-            webpage_url = process_info['webpage_url']
+        if 'entries' not in processed_info:
+            info = processed_info
+        else:
+            info = None
+            while info is None:
+                try:
+                    info = processed_info['entries'].pop(0)
+                except IndexError:
+                    raise YTDLError('Couldn\'t retrieve any matches for `{}`'.format(webpage_url))
 
-            partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=True)
-            processed_info = await loop.run_in_executor(None, partial)
+        return cls(ctx, data=info)
 
-            if processed_info is None:
-                raise YTDLError('Couldn\'t fetch `{}`'.format(webpage_url))
+    @staticmethod
+    def parse_duration(duration: int):
+        minutes, seconds = divmod(duration, 60)
+        hours, minutes = divmod(minutes, 60)
+        days, hours = divmod(hours, 24)
 
-            if 'entries' not in processed_info:
-                info = processed_info
-            else:
-                info = None
-                while info is None:
-                    try:
-                        info = processed_info['entries'].pop(0)
-                    except IndexError:
-                        raise YTDLError('Couldn\'t retrieve any matches for `{}`'.format(webpage_url))
+        duration = []
+        if days > 0:
+            duration.append('{} days'.format(days))
+        if hours > 0:
+            duration.append('{} hours'.format(hours))
+        if minutes > 0:
+            duration.append('{} minutes'.format(minutes))
+        if seconds > 0:
+            duration.append('{} seconds'.format(seconds))
 
-            return cls.ytdl.prepare_filename(info), cls(ctx, data=info)
-
-        @staticmethod
-        def parse_duration(duration: int):
-            minutes, seconds = divmod(duration, 60)
-            hours, minutes = divmod(minutes, 60)
-            days, hours = divmod(hours, 24)
-
-            duration = []
-            if days > 0:
-                duration.append('{} days'.format(days))
-            if hours > 0:
-                duration.append('{} hours'.format(hours))
-            if minutes > 0:
-                duration.append('{} minutes'.format(minutes))
-            if seconds > 0:
-                duration.append('{} seconds'.format(seconds))
-
-            return ', '.join(duration)
-
-    class YTDLInfo:
-        YTDL_OPTIONS = {
-            'format': 'bestaudio/best',
-            'extractaudio': True,
-            'audioformat': 'mp3',
-            'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-            'restrictfilenames': True,
-            'noplaylist': True,
-            'nocheckcertificate': True,
-            'ignoreerrors': False,
-            'logtostderr': False,
-            'quiet': True,
-            'no_warnings': True,
-            'default_search': 'auto',
-            'source_address': '0.0.0.0',
-        }
-
-        FFMPEG_OPTIONS = {
-            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-            'options': '-vn',
-        }
-
-        ytdl = youtube_dl.YoutubeDL(YTDL_OPTIONS)
-
-        def __init__(self, ctx: commands.Context, *, data: dict, volume: float = 0.5):
-
-            self.data = data
-
-            self.uploader = data.get('uploader')
-            self.uploader_url = data.get('uploader_url')
-            date = data.get('upload_date')
-            self.upload_date = date[6:8] + '/' + date[4:6] + '/' + date[0:4]
-            self.title = data.get('title')
-            self.thumbnail = data.get('thumbnail')
-            self.description = data.get('description')
-            self.duration = self.parse_duration(int(data.get('duration')))
-            self.tags = data.get('tags')
-            self.url = data.get('webpage_url')
-            self.views = data.get('view_count')
-            self.likes = data.get('like_count')
-            self.dislikes = data.get('dislike_count')
-            self.stream_url = data.get('url')
-
-        def __str__(self):
-            return '**{0.title}** by **{0.uploader}**'.format(self)
-
-        @classmethod
-        async def create_source(cls, ctx: commands.Context, search: str, *, loop: asyncio.BaseEventLoop = None):
-            loop = loop or asyncio.get_event_loop()
-
-            partial = functools.partial(cls.ytdl.extract_info, search, download=False, process=False)
-            data = await loop.run_in_executor(None, partial)
-
-            if data is None:
-                raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
-
-            if 'entries' not in data:
-                process_info = data
-            else:
-                process_info = None
-                for entry in data['entries']:
-                    if entry:
-                        process_info = entry
-                        break
-
-                if process_info is None:
-                    raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
-
-            webpage_url = process_info['webpage_url']
-            partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=False)
-            processed_info = await loop.run_in_executor(None, partial)
-
-            if processed_info is None:
-                raise YTDLError('Couldn\'t fetch `{}`'.format(webpage_url))
-
-            if 'entries' not in processed_info:
-                info = processed_info
-            else:
-                info = None
-                while info is None:
-                    try:
-                        info = processed_info['entries'].pop(0)
-                    except IndexError:
-                        raise YTDLError('Couldn\'t retrieve any matches for `{}`'.format(webpage_url))
-
-            return cls(ctx, data=info)
-
-        @staticmethod
-        def parse_duration(duration: int):
-            minutes, seconds = divmod(duration, 60)
-            hours, minutes = divmod(minutes, 60)
-            days, hours = divmod(hours, 24)
-
-            duration = []
-            if days > 0:
-                duration.append('{} days'.format(days))
-            if hours > 0:
-                duration.append('{} hours'.format(hours))
-            if minutes > 0:
-                duration.append('{} minutes'.format(minutes))
-            if seconds > 0:
-                duration.append('{} seconds'.format(seconds))
-
-            return ', '.join(duration)
+        return ', '.join(duration)
 
 
 class Player:
