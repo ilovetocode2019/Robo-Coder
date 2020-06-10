737bd5d2274e683c642b4c72707144ff682f8920
Nathan <nathanwj123@outlook.com>
2020-06-10 14:13:41 -0700

    Add docs command

--- cogs/tools.py
+++ cogs/tools.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord.ext import menus
 import discord
 
 from datetime import datetime as d
@@ -10,6 +11,58 @@ import aiohttp
 
 from PIL import Image
 from io import BytesIO
+
+import io
+import re
+import zlib
+
+class DocsPages(menus.ListPageSource):
+    def __init__(self, data, search, ctx):
+        self.search = search
+        self.data = data
+        self.ctx = ctx
+        self.bot = ctx.bot
+        super().__init__(data, per_page=10)
+
+    async def format_page(self, menu, entries):
+        offset = menu.current_page * self.per_page
+        em = discord.Embed(title=f"Results for search '{self.search}'", description="", color=discord.Colour.from_rgb(*self.bot.customization[str(self.ctx.guild.id)]["color"]))
+        for i, v in enumerate(entries, start=offset):
+            em.description += "\n["+v[0]+"]("+v[1]+")"
+        em.set_footer(text=f"{len(self.data)} results | Page {menu.current_page+1}/{int(len(self.data)/10)+1}")
+
+        return em
+class SphinxObjectFileReader:
+    # Inspired by Sphinx's InventoryFileReader
+    BUFSIZE = 16 * 1024
+
+    def __init__(self, buffer):
+        self.stream = io.BytesIO(buffer)
+
+    def readline(self):
+        return self.stream.readline().decode('utf-8')
+
+    def skipline(self):
+        self.stream.readline()
+
+    def read_compressed_chunks(self):
+        decompressor = zlib.decompressobj()
+        while True:
+            chunk = self.stream.read(self.BUFSIZE)
+            if len(chunk) == 0:
+                break
+            yield decompressor.decompress(chunk)
+        yield decompressor.flush()
+
+    def read_compressed_lines(self):
+        buf = b''
+        for chunk in self.read_compressed_chunks():
+            buf += chunk
+            pos = buf.find(b'\n')
+            while pos != -1:
+                yield buf[:pos].decode('utf-8')
+                buf = buf[pos + 1:]
+                pos = buf.find(b'\n')
 
 def snowstamp(snowflake):
     timestamp = (int(snowflake) >> 22) + 1420070400000
@@ -34,10 +87,152 @@ async def average_image_color(avatar_url, loop):
 
     return(discord.Color(int("0x{:02x}{:02x}{:02x}".format(*color), 16)))
 
+def finder(text, collection, *, key=None, lazy=True):
+    suggestions = []
+    text = str(text)
+    pat = '.*?'.join(map(re.escape, text))
+    regex = re.compile(pat, flags=re.IGNORECASE)
+    for item in collection:
+        to_search = key(item) if key else item
+        r = regex.search(to_search)
+        if r:
+            suggestions.append((len(r.group()), r.start(), item))
+
+    def sort_key(tup):
+        if key:
+            return tup[0], tup[1], key(tup[2])
+        return tup
+
+    if lazy:
+        return (z for _, _, z in sorted(suggestions, key=sort_key))
+    else:
+        return [z for _, _, z in sorted(suggestions, key=sort_key)]
+
 class Tools(commands.Cog):
     """Tools for discord."""
     def __init__(self, bot):
         self.bot = bot
+
+    def parse_object_inv(self, stream, url):
+        # key: URL
+        # n.b.: key doesn't have `discord` or `discord.ext.commands` namespaces
+        result = {}
+
+        # first line is version info
+        inv_version = stream.readline().rstrip()
+
+        if inv_version != '# Sphinx inventory version 2':
+            raise RuntimeError('Invalid objects.inv file version.')
+
+        # next line is "# Project: <name>"
+        # then after that is "# Version: <version>"
+        projname = stream.readline().rstrip()[11:]
+        version = stream.readline().rstrip()[11:]
+
+        # next line says if it's a zlib header
+        line = stream.readline()
+        if 'zlib' not in line:
+            raise RuntimeError('Invalid objects.inv file, not z-lib compatible.')
+
+        # This code mostly comes from the Sphinx repository.
+        entry_regex = re.compile(r'(?x)(.+?)\s+(\S*:\S*)\s+(-?\d+)\s+(\S+)\s+(.*)')
+        for line in stream.read_compressed_lines():
+            match = entry_regex.match(line.rstrip())
+            if not match:
+                continue
+
+            name, directive, prio, location, dispname = match.groups()
+            domain, _, subdirective = directive.partition(':')
+            if directive == 'py:module' and name in result:
+                # From the Sphinx Repository:
+                # due to a bug in 1.1 and below,
+                # two inventory entries are created
+                # for Python modules, and the first
+                # one is correct
+                continue
+
+            # Most documentation pages have a label
+            if directive == 'std:doc':
+                subdirective = 'label'
+
+            if location.endswith('$'):
+                location = location[:-1] + name
+
+            key = name if dispname == '-' else dispname
+            prefix = f'{subdirective}:' if domain == 'std' else ''
+
+            if projname == 'discord.py':
+                key = key.replace('discord.ext.commands.', '').replace('discord.', '')
+
+            result[f'{prefix}{key}'] = os.path.join(url, location)
+
+        return result
+
+    async def build_docs_lookup_table(self, page_types):
+        cache = {}
+        for key, page in page_types.items():
+            sub = cache[key] = {}
+            async with aiohttp.ClientSession().get(page + "/objects.inv") as resp:
+                if resp.status != 200:
+                    raise RuntimeError(
+                        "Cannot build docs lookup table, try again later."
+                    )
+
+                stream = SphinxObjectFileReader(await resp.read())
+                cache[key] = self.parse_object_inv(stream, page)
+
+        self._docs_cache = cache
+
+    async def do_docs(self, ctx, key, obj):
+        page_types = {
+            "latest": "https://discordpy.readthedocs.io/en/latest",
+            "python": "https://docs.python.org/3"
+        }
+
+        if obj is None:
+            await ctx.send(page_types[key])
+            return
+
+        if not hasattr(self, "_docs_cache"):
+            await ctx.trigger_typing()
+            await self.build_docs_lookup_table(page_types)
+
+        obj = re.sub(r"^(?:discord\.(?:ext\.)?)?(?:commands\.)?(.+)", r"\1", obj)
+
+        if key.startswith("latest") or key.startswith("stable"):
+            # point the abc.Messageable types properly:
+            q = obj.lower()
+            for name in dir(discord.abc.Messageable):
+                if name[0] == "_":
+                    continue
+                if q == name:
+                    obj = f"abc.Messageable.{name}"
+                    break
+
+        cache = list(self._docs_cache[key].items())
+
+        def transform(tup):
+            return tup[0]
+
+        matches = finder(obj, cache, key=lambda t: t[0], lazy=False)
+
+        if len(matches) == 0:
+            return await ctx.send("Could not find anything. Sorry.")
+        
+        em = discord.Embed(title=f"Results for search '{obj}'", description="")
+        for match in matches[:5]:
+            em.description += "\n["+match[0]+"]("+match[1]+")"
+        """await ctx.send(embed=em)"""
+        pages = menus.MenuPages(source=DocsPages(matches, obj, ctx), clear_reactions_after=True)
+        await pages.start(ctx)
+
+    @commands.group(name="docs", description="Get a link for docs on discord.py", usage="[object]", invoke_without_command=True)
+    async def docs(self, ctx, obj=None):
+        await self.do_docs(ctx, "latest", obj)
+
+    @docs.command(name="py", description="Get a link for docs on python", usage="[object]")
+    async def py_docs(self, ctx, obj=None):
+        await self.do_docs(ctx, "python", obj)
     
     @commands.command(name="source", descriptin="Get source code for a specified command", usage="[command]")
     async def sourcecode(self, ctx, *, command):
