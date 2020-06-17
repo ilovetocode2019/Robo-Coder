2b3a2d3177a8607e3964d518c8f11ae1dfddc9b9
Nathan <nathanwj123@outlook.com>
2020-06-16 19:37:52 -0700

    Make help command case insensitive

--- cogs/meta.py
+++ cogs/meta.py
@@ -196,7 +196,50 @@ class RoboCoderHelpCommand(commands.HelpCommand):
             embed.add_field(name="Aliases:", value=", ".join(command.aliases), inline=False)
         await ctx.send(embed=embed)
 
+    async def command_callback(self, ctx, *, command=None):
+        #Overiding this so I can have case insensitivity for cog help
+        command = command.capitalize()
+ 
+        await self.prepare_help_command(ctx, command)
+        bot = ctx.bot
 
+        if command is None:
+            mapping = self.get_bot_mapping()
+            return await self.send_bot_help(mapping)
+
+        # Check if it's a cog
+        cog = bot.get_cog(command)
+        if cog is not None:
+            return await self.send_cog_help(cog)
+
+        maybe_coro = discord.utils.maybe_coroutine
+
+        # If it's not a cog then it's a command.
+        # Since we want to have detailed errors when someone
+        # passes an invalid subcommand, we need to walk through
+        # the command group chain ourselves.
+        keys = command.split(' ')
+        cmd = bot.all_commands.get(keys[0])
+        if cmd is None:
+            string = await maybe_coro(self.command_not_found, self.remove_mentions(keys[0]))
+            return await self.send_error_message(string)
+
+        for key in keys[1:]:
+            try:
+                found = cmd.all_commands.get(key)
+            except AttributeError:
+                string = await maybe_coro(self.subcommand_not_found, cmd, self.remove_mentions(key))
+                return await self.send_error_message(string)
+            else:
+                if found is None:
+                    string = await maybe_coro(self.subcommand_not_found, cmd, self.remove_mentions(key))
+                    return await self.send_error_message(string)
+                cmd = found
+
+        if isinstance(cmd, commands.Group):
+            return await self.send_group_help(cmd)
+        else:
+            return await self.send_command_help(cmd)
 
 class Meta(commands.Cog):
     """Everything about the bot itself."""
