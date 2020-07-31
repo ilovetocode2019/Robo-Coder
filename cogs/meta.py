61976993a067711776776ae1681fa7784cee0fd9
Nathan <nathanwj123@outlook.com>
2020-07-31 09:01:18 -0700

    Don't show overall uptime

--- cogs/meta.py
+++ cogs/meta.py
@@ -369,7 +369,7 @@ class Meta(commands.Cog):
         connected_time = datetime.now()-self.bot.connected_at
 
 
-        await ctx.send(f"I started up {uptime.days} days, {time.readable(uptime.seconds)} ago \n{await self.get_overall_uptime()}")  
+        await ctx.send(f"I started up {uptime.days} days, {time.readable(uptime.seconds)} ago")  
 
     
     @commands.command(name="invite", description="Get a invite to add me to your server")
