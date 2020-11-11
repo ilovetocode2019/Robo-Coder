b6f6987a678025cfd05052e2353c74ea2d92d5a1
ilovetocode2019 <54194229+ilovetocode2019@users.noreply.github.com>
2020-11-11 09:00:47 -0800

    Add some more fun commands

--- cogs/fun.py
+++ cogs/fun.py
@@ -11,12 +11,25 @@ class Fun(commands.Cog):
         self.emoji = ":tada:"
 
     @commands.command(name="flipcoin", description="Flip a coin")
-    async def flipcoin(self, ctx):
-        await ctx.send(f"You got a {random.choice(['head', 'tail'])}")
+    async def flipcion(self, ctx):
+        await ctx.send(random.choice(["Heads", "Tails"]))
 
-    @commands.command(name="8ball", description="Use a 8 ball")
-    async def magic8ball(self, ctx):
-        await ctx.send(f"{random.choice(['Yes', 'No', 'Maybe'])}")
-    
+    @commands.command(name="random", description="Choose a random option", aliases=["choose", "which", "select"])
+    async def select(self, ctx, *options):
+        await ctx.send(random.choice(options))
+
+    @commands.command(name="rolldice", description="Role some dice")
+    async def rolldice(self, ctx, dice=1, sides=6):
+        numbers = [str(random.randint(1, sides)) for x in range(dice)]
+        if len(numbers) == 1:
+            dice_str = str(numbers[0])
+        else:
+            dice_str = f"{', '.join(numbers[:-1])} and a {numbers[-1]}"
+        await ctx.send(f"You roled a {dice_str}")
+
+    @commands.command(name="question", description="Ask me a question", aliases=["yesno", "8ball"])
+    async def question(self, ctx, *, question):
+        await ctx.send(random.choice(["Yes", "No", "Maybe"]))
+
 def setup(bot):
     bot.add_cog(Fun(bot))
