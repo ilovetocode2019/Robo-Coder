62ffc44db365c409a1e53b986484b8f5f87283c3
ilovetocode2019 <54194229+ilovetocode2019@users.noreply.github.com>
2020-11-16 19:36:55 -0800

    Fix typo

--- cogs/internet.py
+++ cogs/internet.py
@@ -333,7 +333,7 @@ class Internet(commands.Cog):
 
         await ctx.send(embed=em, file=discord.File(output, filename="face.png"))
 
-    @commands.command(name="wikipedia", descrption="Search wikipedia", aliases=["wiki"])
+    @commands.command(name="wikipedia", descroption="Search wikipedia", aliases=["wiki"])
     @commands.cooldown(2, 30, commands.BucketType.user)
     async def wikipedia(self, ctx, *, search):
         url = "http://en.wikipedia.org/w/api.php"
