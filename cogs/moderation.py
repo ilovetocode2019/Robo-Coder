5f8776ed8dd9c1e3321c2e46395c9ebc2056709f
ilovetocode2019 <54194229+ilovetocode2019@users.noreply.github.com>
2020-11-22 09:11:00 -0800

    Fix typo

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -303,7 +303,7 @@ class Moderation(commands.Cog):
         delta = time-datetime.datetime.utcnow()
         if delta > datetime.timedelta(days=1):
             return await ctx.send(":x: You cannot mute yourself for more than a day")
-        if delta < datetime.timedelta(minutes=5):
+        if delta+datetime.timedelta(seconds=1) <= datetime.timedelta(minutes=5):
             return await ctx.send(":x: You must mute yourself for at least 5 minutes")
 
         if not config.mute_role:
@@ -314,7 +314,7 @@ class Moderation(commands.Cog):
         if reason:
             reason = f"Selfmute by {ctx.author} with reason {reason}"
         else:
-            reason = f"Selfmute by {ctx.autor}"
+            reason = f"Selfmute by {ctx.author}"
 
         timers = self.bot.get_cog("Timers")
         if not timers:
