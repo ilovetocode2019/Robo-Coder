eea50f05d624937ff52cf27c9bfdb98f9fcc7128
ilovetocode2019 <54194229+ilovetocode2019@users.noreply.github.com>
2020-12-09 11:41:56 -0800

    Fix issue with leave_on_inactivity

--- cogs/music.py
+++ cogs/music.py
@@ -1014,7 +1014,7 @@ class Music(commands.Cog):
         player.voice.pause()
 
         def check(member, before, after):
-            return not member.bot and after.channel and after.channel == player.voice.channel
+            return (not member.bot and after.channel and after.channel == player.voice.channel) or (member.id == self.bot.user.id and before.channel != after.channel)
 
         try:
             await self.bot.wait_for("voice_state_update", timeout=180, check=check)
