f5d6963fc36eb85b08ce15fba971c0934a88c077
ilovetocode <54194229+ilovetocode2019@users.noreply.github.com>
2025-02-18 21:53:46 -0800

    Refactor fun cog

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,73 +1,63 @@
-import random
-
 import discord
 from discord.ext import commands
 
+import random
+
 from .utils import formats
 
+
 class Fun(commands.Cog):
-    """Fun commands."""
+    """Surprisingly entertaining random generators."""
+
+    emoji = "\N{PARTY POPPER}"
 
     def __init__(self, bot):
         self.bot = bot
-        self.emoji = ":tada:"
 
-    @commands.hybrid_command(name="flipcoin", description="Flip a coin")
+    @commands.hybrid_command(description="Flips a virtual coin to help you make a decision")
     async def flipcoin(self, ctx):
         result = random.choice(["Heads", "Tails"])
         await ctx.send(f":coin: You flipped {result}")
 
-    @commands.hybrid_command(name="rolldice", description="Roll some dice", aliases=["rolldie"])
-    async def rolldice(self, ctx, dice=1, sides=6):
-        if not dice:
-            return await ctx.send(":x: You must roll at least 1 die")
-        elif not sides:
-            return await ctx.send(f":x: Your {'die' if dice == 1 else 'dice'} must have sides")
+    @commands.hybrid_command(description="Rolls some custom die", aliases=["rolldice"])
+    async def rolldie(self, ctx, die: commands.Range[int, 1, 10] = 1, sides: commands.Range[int, 2, 100] = 6):
+        numbers = [str(random.randint(1, sides)) for x in range(die)]
+        await ctx.send(f":game_die: You rolled {formats.join(numbers, last="and")}")
 
-        if dice > 10:
-            return await ctx.send(":x: You can't roll more than 10 dice")
-        elif sides > 100000:
-            return await ctx.send(f":x: Your {'die' if dice == 1 else 'dice'} can't have more than 100,000 sides") 
-
-        numbers = [str(random.randint(1, sides)) for x in range(dice)]
-        await ctx.send(f":game_die: You rolled {formats.join(numbers, last='and')}")
-
-    @commands.hybrid_command(name="8ball", description="Ask me a question", aliases=["eightball"])
-    async def eightball(self, ctx, question):
-        choice = random.choice(
-            [
-                "Yes",
-                "Certainly",
-                "Obviously",
-                "Of course",
-                "For sure",
-                "Without a doubt",
-                "It's likely"
-                "No",
-                "Nope",
-                "No way"
-                "Not a change",
-                "Definitely not",
-                "Obviously not",
-                "Certainly not",
-                "It's unlikely",
-                "Maybe",
-                "Quite possibly",
-                "There is a chance",
-                "It could go either way",
-                "It's possible",
-            ]
-        )
-
+    @commands.hybrid_command(name="8ball", description="Answers whatever question you'd like", aliases=["eightball"])
+    async def eightball(self, ctx, *, question):
+        choice = random.choice([
+            "Yes",
+            "Certainly",
+            "Obviously",
+            "Of course",
+            "For sure",
+            "Without a doubt",
+            "It's likely"
+            "No",
+            "Nope",
+            "No way"
+            "Not a change",
+            "Definitely not",
+            "Obviously not",
+            "Certainly not",
+            "It's unlikely",
+            "Maybe",
+            "Quite possibly",
+            "There is a chance",
+            "It could go either way",
+            "It's possible"
+        ])
         await ctx.reply(choice)
 
     @commands.command(name="choose", description="Choose a random option", aliases=["choice"])
     async def choose(self, ctx, *options):
-        if not options:
-            return await ctx.send(":x: You must specify options to choose from")
+        if len(options) < 2:
+            return await ctx.send("\N{CROSS MARK} You must specify at least two choices")
 
         choice = random.choice(options)
         await ctx.send(choice)
+
 
 async def setup(bot):
     await bot.add_cog(Fun(bot))
